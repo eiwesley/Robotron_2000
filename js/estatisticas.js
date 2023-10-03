c4f162ec07a892a1c9d5ddc3e934d61c43112bbb
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-10-03 20:35:53 -0300

    :sparkles: feat: 5.2 Interagindo com objetos

--- js/estatisticas.js
+++ js/estatisticas.js
@@ -1,4 +1,3 @@
-const controle = document.querySelectorAll("[data-controle")
 const pecas = {
     "bracos": {
         "forca": 29,
@@ -30,21 +29,5 @@ const pecas = {
         "poder": 28,
         "energia": 0,
         "velocidade": -2
-    }
-}
-
-controle.forEach((elemento) => {
-    elemento.addEventListener("click", (evento) => {
-    manipulaDados(evento.target.dataset.controle,evento.target.parentNode)
-    })
-})
-
-function manipulaDados(operacao, controle) {
-    const peca = controle.querySelector("[data-contador]")
-
-    if (operacao === "-") {
-        peca.value = parseInt(peca.value) - 1
-    } else {
-        peca.value = parseInt(peca.value) + 1
     }
 }
