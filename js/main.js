71cfe7a53e0e4b2e7c0333e3faf3919b26d765f7
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-10-02 21:05:30 -0300

    :sparkles: feat: 2.2 Eventos com JavaScript

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const robotron = document.querySelector("#robotron") // busca o elemento através do queryselector
+
+robotron.addEventListener("click", dizOi)
+
+function dizOi(){
+    console.log("Oi")
+}
