560629973e824e98846153530fc4c6cb047e17e4
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-10-02 21:15:18 -0300

    :sparkles: feat: 2.4 Funções com JavaScript

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const robotron = document.querySelector("#robotron") // busca o elemento através do queryselector
 
-robotron.addEventListener("click", dizOi)
+robotron.addEventListener("click", () => { // função anonima de seta
+    console.log('Cliquei no robo')
+}) 
 
-function dizOi(){
-    console.log("Oi")
+function dizOi(nome){
+    console.log("Oi ", nome)
+    console.log("Bem-Vindo ao Robotron 2000")
 }
+
+dizOi('Wesley')
